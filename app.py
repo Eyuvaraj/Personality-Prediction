4fcf60e8a98e6bbd343dd0d5e6319f8a765a70b1
Eyuvaraj <102822607+Eyuvaraj@users.noreply.github.com>
2023-12-03 23:52:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-import streamlit as st
 
-placeholder = st.empty()
-
-with placeholder.container():
-    with st.form("my_form"):
-        st.write("Inside the form")
-        my_number = st.slider("Pick a number", 1, 10)
-        my_color = st.selectbox(
-            "Pick a color", ["red", "orange", "green", "blue", "violet"]
-        )
-        submit = st.form_submit_button("Submit my picks")
-    if submit:
-        placeholder.text("sfs")
